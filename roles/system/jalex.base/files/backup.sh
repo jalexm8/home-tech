6f64b195b3106f8894eb4c13b88d40764a3f73f6
jalexm8 <jack.alexander1008@gmail.com>
2022-05-14 11:21:58 +0100

    Changed backup script

--- roles/system/jalex.base/files/backup.sh
+++ roles/system/jalex.base/files/backup.sh
@@ -4,12 +4,12 @@ if [[ ! -d ~/archive ]]; then
     mkdir ~/archive
 fi
 
-if tar cvzf ~/archive/src.tar.gz ~/src >> /dev/null 2>&1; then
-    if rsync -a -e 'ssh -p 69' ~/archive/src.tar.gz 192.168.0.68:/opt/plexmedia/backup >> /dev/null 2>&1; then
-        notify-send 'BACKUP COMPLETE' 'Successful backup sent to remote computer.'
-    else
-        notify-send 'RSYNC FAIL' 'rsync did not copy to remote backup.'
-    fi
-else
+if ! tar cvzf ~/archive/src.tar.gz ~/src >> /dev/null 2>&1; then
     notify-send 'TAR FAIL' 'The tar command failed, cannot backup.'
+    exit 1
+fi
+
+if ! rsync -a -e 'ssh -p 69' ~/archive/src.tar.gz 192.168.0.68:/opt/plexmedia/backup >> /dev/null 2>&1; then
+    notify-send 'RSYNC FAIL' 'rsync did not copy to remote backup.'
+    exit 1
 fi
